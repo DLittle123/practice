5d672e92839054a28666852fadf748d85ed6a473
dlittle123 <dlittle1@gmail.com>
2018-10-16 21:41:21 -0400

    add env vars, finished brute force checking
    added pretty print

--- Readme.md
+++ Readme.md
@@ -1,7 +1,14 @@
-To Run the program
+This runs on python 3.6.3
 
-`source venv/bin/activate`
-`python3 main.py`
+Wedding seat planner
+
+Optional arguments `TABLENAME={filename in input_data} PARTYNAME={filename in input_data}`
+
+To run
+
+`{optional arguments} python3 main.py`
 
 
 Also, this is a variation of the bin packing problem, with no generalized solution. 
+
+The approach centers on running through 2 simple solutions first followed by a brute force solution
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ Author: David Little
 """
 import re
 import copy
+import argparse
+import os
 
 """
 Reads in the table info from a file
@@ -12,12 +14,12 @@ assumptions:
   'tables: ' is always at the head
   tables are defined as 'table_name-max_capacity' seperated by a space
 """
-
-def load_table_info(filename='tables.txt'):
+def load_table_info(filename):
   with open("input_data/" + filename, "r") as input_tables:
     table_array = input_tables.readline().split(': ')[1].split(' ')
     #return { x.split('-')[0]: x.split('-')[1] for x in table_array }
     return list(map(lambda x: {'name': x.split('-')[0], 'size_remaining': int(x.split('-')[1]), 'parties': []}, table_array))
+
 
 """
 Reads in party info from a file
@@ -25,8 +27,7 @@ assumptions:
   reservations info is always one party per line 
   format is "party_name, party of \d, (dislikes party_name(, party_name)*)?"
 """
-
-def load_party_info(filename='reservations.txt'):
+def load_party_info(filename):
   party_array = []
   for party in open("input_data/" + filename, "r"):
     split_party = re.split(r', party of | dislikes ', party.rstrip('\n'))
@@ -47,11 +48,11 @@ def sorter(list_, keyword, reverse=False):
 """
 main controller
 """
-def main():
+def main(partyname, tablename):
   solution = None
   
-  table_info = load_table_info()
-  party_info = load_party_info()
+  table_info = load_table_info(tablename)
+  party_info = load_party_info(partyname)
   
   sorted_tables_small_first = sorter(copy.deepcopy(table_info), 'size_remaining')
   sorted_tables_big_first = sorter(copy.deepcopy(table_info), 'size_remaining', reverse=True)
@@ -60,10 +61,10 @@ def main():
   solution = simple_solution(sorted_tables_big_first.copy(), sorted_parties.copy())
   if not solution:
     solution = simple_solution(sorted_tables_small_first.copy(), sorted_parties.copy())
-  elif not solution:
-    all_solutions = generate_all_solutions(copy.deepcopy(table_info), copy.deepcopy(party_info))
-    solution = find_a_solution(all_solutions, table_info.copy())
-  print(solution)
+    if not solution:
+      all_solutions = generate_all_solutions(copy.deepcopy(table_info), copy.deepcopy(party_info))
+      solution = find_a_solution(all_solutions, table_info.copy())
+  print(solution) if type(solution) == str else pretty_print(solution)
 
 
 """
@@ -133,17 +134,43 @@ Lifted from https://stackoverflow.com/questions/34030873/count-upward-in-python-
 Essentially uses recursion to build up a string representing the number in a variable base
 Works up to base26 (I hope we don't have more than 26 tables, otherwise we need to rejigger function)
 """
-
 def int2str(x, base):
   digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   if x < 0:
     return "-" + int2str(-x, base)
   return ("" if x < base else int2str(x//base, base)) + digits[x % base]
 
+
 """
 find the first solution that works from a solution array
 """
-def find_a_solution(solution_array):
+def find_a_solution(solution_array, tables):
+  for solution in solution_array:
+    table_copy = copy.deepcopy(tables)
+    viable_solution = True
+    for index, group in enumerate(solution):
+      for party in group:
+        if not fits(party, table_copy[index]) or not sits(party, table_copy[index]['parties']):
+          viable_solution = False
+        else:
+          table_copy[index]['parties'].append(party)
+          table_copy[index]['size_remaining'] -= party['size']
+    if viable_solution:
+      return table_copy
+  return "No viable solution"
 
 
+"""
+Formats a solution nicely for printing
+"""
+def pretty_print(solution):
+  for table in solution:
+    header = "Table {name}: ".format(name=table['name'])
+    if table['parties']:
+      party_string = ' & '.join(list("{name}, party of {size}".format(name=party['name'], size=party['size']) for party in table['parties']))
+    else:
+      party_string = 'Empty' 
+    print(header+party_string)
 
+if __name__ == "__main__":
+  main(os.environ.get('PARTYNAME', 'reservations.txt'), os.environ.get('TABLENAME', 'tables.txt'))
